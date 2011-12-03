4809086634c7c306697021c2daa0029430fb318c
Chris Burris <chris@chilitechno.com>
2011-12-03 15:19:53 -0500

    Updates
    Updated README with additional information.
    Code cleanup.

--- lib/siriproxy-nestlearningthermostat.rb
+++ lib/siriproxy-nestlearningthermostat.rb
@@ -18,6 +18,7 @@ class SiriProxy::Plugin::NestLearningThermostat < SiriProxy::Plugin
     listen_for(/status.*nest/i) { show_status_of_thermostat }
     
     listen_for(/thermostat.*([0-9]{2})/i) { |temp| set_thermostat(temp) }
+    listen_for(/nest.*([0-9]{2})/i) { |temp| set_thermostat(temp) }
         
     def show_status_of_thermostat
         say "Checking the status of the Nest."
@@ -42,12 +43,15 @@ class SiriProxy::Plugin::NestLearningThermostat < SiriProxy::Plugin
                 
                 if statusResult
                     structure_id = statusResult["user"][user_id]["structures"][0].split('.')[1]
-                    device_id = statusResult["structure"][structure_id]["devices"][0].split('.')[1]
-                    current_temp = (statusResult["shared"][device_id]["current_temperature"] * 1.8) + 32
+                    device_serial_id = statusResult["structure"][structure_id]["devices"][0].split('.')[1]
+                    # devices element could contain multiple serial_numbers if multiple thermostats associated to nest account. 
+                    # serial number is something like 01AB23CD456789EF
+                    
+                    current_temp = (statusResult["shared"][device_serial_id]["current_temperature"] * 1.8) + 32
                     current_temp = current_temp.round
-                    target_temp = (statusResult["shared"][device_id]["target_temperature"] * 1.8) + 32
+                    target_temp = (statusResult["shared"][device_serial_id]["target_temperature"] * 1.8) + 32
                     target_temp = target_temp.round
-                    thermostat_name = statusResult["shared"][device_id]["name"]
+                    thermostat_name = statusResult["shared"][device_serial_id]["name"]
                     say "The #{thermostat_name} Nest is currently set to #{target_temp} degrees. The current temperature is #{current_temp} degrees."
                 else
                     say "Sorry, I couldn't understand the response from Nest.com"
@@ -89,22 +93,22 @@ class SiriProxy::Plugin::NestLearningThermostat < SiriProxy::Plugin
                 
                 if statusResult
                     structure_id = statusResult["user"][user_id]["structures"][0].split('.')[1]
-                    device_id = statusResult["structure"][structure_id]["devices"][0].split('.')[1]
-                    version_id = statusResult["shared"][device_id]["$version"]
-                    current_temp = (statusResult["shared"][device_id]["current_temperature"] * 1.8) + 32
+                    device_serial_id = statusResult["structure"][structure_id]["devices"][0].split('.')[1]
+                    version_id = statusResult["shared"][device_serial_id]["$version"]
+                    current_temp = (statusResult["shared"][device_serial_id]["current_temperature"] * 1.8) + 32
                     current_temp = current_temp.round
-                    thermostat_name = statusResult["shared"][device_id]["name"]
+                    thermostat_name = statusResult["shared"][device_serial_id]["name"]
                     
                     target_temp_celsius = (temp.to_f - 32.0) / 1.8
                     target_temp_celsius = target_temp_celsius.round(5)
                     
                     payload = '{"target_change_pending":true,"target_temperature":' + "#{target_temp_celsius}" + '}'
                     puts payload
-                    puts device_id
+                    puts device_serial_id
                     puts version_id
-                    puts 'POST ' + transport_url + '/v2/put/shared.' + device_id
+                    puts 'POST ' + transport_url + '/v2/put/shared.' + device_serial_id
                     begin
-                        tempRequest = HTTParty.post(transport_url + '/v2/put/shared.' + device_id, :body => payload, :headers => { 'Host' => transport_host, 'User-Agent' => 'Nest/1.1.0.10 C.10 CFNetwork/548.0.4', 'Authorization' => 'Basic ' + access_token, 'X-nl-protocol-version' => '1'})
+                        tempRequest = HTTParty.post(transport_url + '/v2/put/shared.' + device_serial_id, :body => payload, :headers => { 'Host' => transport_host, 'User-Agent' => 'Nest/1.1.0.10 C.10 CFNetwork/548.0.4', 'Authorization' => 'Basic ' + access_token, 'X-nl-protocol-version' => '1'})
                     rescue
                         puts 'error: ' 
                     end
